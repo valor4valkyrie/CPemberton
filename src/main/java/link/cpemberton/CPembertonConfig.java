9021649d227610199b7206ce88c3d062a1718b85
valor4valkyrie <c.george.pemberton@gmail.com>
2018-09-02 11:13:20 -0500

    Setup a basic intro page

--- src/main/java/link/cpemberton/CPembertonConfig.java
+++
@@ -1,5 +0,0 @@
-package link.cpemberton;
-
-
-public class CPembertonConfig {
-}
--- src/main/java/link/cpemberton/home/BaseController.java
+++ src/main/java/link/cpemberton/home/BaseController.java
@@ -1,16 +1,21 @@
 package link.cpemberton.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BaseController {
 
     @GetMapping("/")
-    public ModelAndView home(ModelAndView mav){
-        String test = "Test";
-        mav.addObject(test);
-        return mav;
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/greet")
+    public String greetings(Model model) {
+        model.addAttribute("name", "Chris");
+        return "greetings";
     }
 }
